983e1cc403a6431b3f70f65c647ec2c01309cf5a
Jens Petersen <juhpetersen@gmail.com>
2021-12-09 15:49:29 +0800

    debug short option was conflicting with --exclude-devel (-D)
    though maybe more natural to use -D for debug

--- src/Main.hs
+++ src/Main.hs
@@ -45,7 +45,7 @@ main = do
     ("HUB = " ++ intercalate ", " knownHubs) $
     program
     <$> switchWith 'n' "dry-run" "Don't actually download anything"
-    <*> switchWith 'D' "debug" "More detailed output"
+    <*> switchWith 'd' "debug" "More detailed output"
     <*> optional (strOptionWith 'H' "hub" "HUB"
                   "KojiHub shortname or url [default: fedora]")
     <*> optional (strOptionWith 'P' "packages-url" "URL"
