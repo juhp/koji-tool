a2a232f0fe8862db44abce9039936f446c67cf12
Jens Petersen <juhpetersen@gmail.com>
2022-02-28 18:01:21 +0800

    tests: add tasks and builds testcases

--- test/tests.hs
+++ test/tests.hs
@@ -1,21 +1,33 @@
 import SimpleCmd
 import System.IO
 
-program :: [String] -> IO ()
-program args =
+program :: ([String], [[String]]) -> IO ()
+program (c, argsv) =
   putStrLn "" >>
-  cmdLog "koji-tool" ("install" : "-n" : args)
+  mapM_ (cmdLog "koji-tool" . (c ++)) argsv
 
-tests :: Bool -> [[String]]
+tests :: Bool -> [([String], [[String]])]
 tests havedist =
-  [["podman", "-p", "podman"] ++ sysdist
-  ,["-l", "coreutils"] ++ sysdist
-  ,["-l", "-R", "rpmlint-2.2.0-1.fc36"]
-  ,["-H", "https://kojihub.stream.centos.org/kojihub", "-d", "el9", "bash", "-p", "bash"]
-  ,["-H", "stream", "-d", "el9", "kernel", "-x", "kernel-devel*", "-x", "*-debug*"]
-  ,["-l", "-H", "stream", "-d", "el9", "grep"]
-  ,["-H", "rpmfusion", "ffmpeg", "-p", "ffmpeg", "-p", "ffmpeg-libs"] ++ sysdist
-  ,["-l", "-H", "rpmfusion", "ffmpeg"] ++ sysdist
+  [
+    (["install", "-n"],
+     [["podman", "-p", "podman"] ++ sysdist
+     ,["-l", "coreutils"] ++ sysdist
+     ,["-l", "-R", "rpmlint-2.2.0-1.fc36"]
+     ,["-H", "https://kojihub.stream.centos.org/kojihub", "-d", "el9", "bash", "-p", "bash"]
+     ,["-H", "stream", "-d", "el9", "kernel", "-x", "kernel-devel*", "-x", "*-debug*"]
+     ,["-l", "-H", "stream", "-d", "el9", "grep"]
+     ,["-H", "rpmfusion", "ffmpeg", "-p", "ffmpeg", "-p", "ffmpeg-libs"] ++ sysdist
+     ,["-l", "-H", "rpmfusion", "ffmpeg"] ++ sysdist])
+  ,
+    (["tasks"],
+     [["-L"]
+     ,["-l", "3"]
+     ,["-L", "-p", "rpm-ostree"]])
+  ,
+    (["builds"],
+     [["-L", "-p", "rust"]
+     ,["-l", "3"]
+     ,["-L", "rpm-ostree*.fc36"]])
   ]
   where
     sysdist = if havedist then [] else ["-d", "fc35"]
